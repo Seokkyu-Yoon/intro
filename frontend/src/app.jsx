8b60698ec47829de83412930c90db526ec29e353
Seokkyu-Yoon <yskyu93@gmail.com>
2022-01-15 13:11:47 +0900

    fix(git-merge): solve problems on merge

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Introduce from './components/introduce';
 import Career from './components/career';
 
